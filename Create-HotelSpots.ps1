e978ef74762d372e908b801dc963bcd3ccda0794
JustinCredible- <JustinCredible-@users.noreply.github.com>
2022-04-29 11:58:58 -0600

    Update Create-HotelSpots.ps1

--- Create-HotelSpots.ps1
+++ Create-HotelSpots.ps1
@@ -7,10 +7,10 @@ $totalspots = 8
 #Change this to however many minutes you want people to be able to schedule the hotel spot for
 $maxappttime = 1440
 
-Connect-MSOnline
+Connect-ExchangeOnline
 while ($i -le $totalspots) {
   $i = $i + 1
   $displayname = 'Hotel Spot ' + $i
   #Don't forget to remove -WhatIf switch or this will do nothing
-  New-Mailbox -Name $displayname.replace(" ","") -DisplayName $displayname -Room -ResourceCapacity 1 | Set-CalendarProcessing -ScheduleOnlyDuringWorkHours $true -MaximumDurationInMinutes $maxappttime -WhatIf
+  New-Mailbox -Name $displayname.replace(" ","") -DisplayName $displayname -Room -ResourceCapacity 1 -WhatIf | Set-CalendarProcessing -ScheduleOnlyDuringWorkHours $true -MaximumDurationInMinutes $maxappttime -WhatIf
 }
